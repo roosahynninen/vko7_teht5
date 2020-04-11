7b53b04f4d142dfa254297fa32aed204b0612848
roosahynninen <63446491+roosahynninen@users.noreply.github.com>
2020-04-11 15:21:12 +0300

    Finished 7.3.

--- app/src/main/java/com/example/vko7_teht1/MainActivity.java
+++ app/src/main/java/com/example/vko7_teht1/MainActivity.java
@@ -4,22 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this.printHello();
-        text = (TextView) findViewById(R.id.textView);
-        text.setText("Hello World!");
+        text = (TextView) findViewById(R.id.textView1);
+        edit   = (EditText)findViewById(R.id.editText1);
     }
 
-    public void printHello(View v) {
-        System.out.println("Hello world!");
+    public void saySomething(View v) {
+        String s;
+        s = edit.getText().toString();
+        text.setText(s);
     }
 }
