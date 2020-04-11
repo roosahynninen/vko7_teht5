e0be1b3a5ed296719051828b64ef39207a62d784
roosahynninen <63446491+roosahynninen@users.noreply.github.com>
2020-04-11 16:20:47 +0300

    Finished 7.4.

--- app/src/main/java/com/example/vko7_teht1/MainActivity.java
+++ app/src/main/java/com/example/vko7_teht1/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.view.View.OnKeyListener;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
     EditText edit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.textView1);
         edit   = (EditText)findViewById(R.id.editText1);
-    }
 
-    public void saySomething(View v) {
-        String s;
-        s = edit.getText().toString();
-        text.setText(s);
+        edit.setOnKeyListener(new OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+                    String s;
+                    s = edit.getText().toString();
+                    text.setText(s);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
